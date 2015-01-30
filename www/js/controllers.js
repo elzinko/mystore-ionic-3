5639632c18a0365470d7ed4d2330362d3d0d8760
Thomas Couderc <thomas.couderc@gmail.com>
2015-01-30 03:14:55 +0100

    move image loader from controller to factory

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -17,73 +17,18 @@ angular.module('starter.controllers', ['ionic'])
         }
     })
 
-    .controller('GalleryCtrl', function ($scope) {
-        $scope.photos = [];
-        $scope.more = true;
-
-        $scope.loadMorePhotos = function () {
-            console.log("loadMorePhotos");
-
-            if ($scope.more) {
-                console.log("need more");
-                var l = $scope.photos.length;
-                if (l >= 60) {
-                    $scope.more = false;
-                    console.log("no More Items Available");
-                } else {
-                    var data = [];
-                    for (var i = l; i < l + 4; i++) {
-                        var height = ~~(Math.random() * 500) + 100;
-                        var width = 280;
-                        var padding = (height / width) * 100;
-                        var id = ~~(Math.random() * 10000);
-                        data.push({
-                            "url": 'http://lorempixel.com/g/' + width + '/' + height + '/?' + id,
-                            "height": height,
-                            "width": width,
-                            "padding": padding
-                        });
-                    }
-                    /*angular.forEach(data, function (one) {
-                     $scope.items.push(one);
-                     });*/
-                    $scope.photos = $scope.photos.concat(data);
-                    $scope.$broadcast('scroll.infiniteScrollComplete');
-                }
-            }
-        };
-
+    .controller('GalleryCtrl', function ($scope, Gallery) {
+        //var gal = new Gallery();
+        //$scope.Gallery = gal;
+        //$scope.loadMore = function() {
+        //    gal.loadMorePhotos();
+        //    console.log('$broadcast scroll.infiniteScrollComplete');
+        //    $scope.$broadcast('scroll.infiniteScrollComplete');
+        //}
+        $scope.Gallery = new Gallery();
+        $scope.loadMore = function() {
+            $scope.Gallery.loadMorePhotos();
+            console.log('$broadcast scroll.infiniteScrollComplete');
+            $scope.$broadcast('scroll.infiniteScrollComplete');
+        }
     });
-
-    //.controller('GalleryCtrl', function ($scope) {
-    //    $scope.items = [];
-    //    $scope.noMoreItemsAvailable = false;
-    //
-    //    $scope.loadMore = function() {
-    //        console.log("  loadMore ...");
-    //
-    //        if (!$scope.noMoreItemsAvailable) {
-    //            var l = $scope.items.length;
-    //            if (l >= 60) {
-    //                $scope.noMoreItemsAvailable = true;
-    //                console.log("no More Items Available");
-    //            } else {
-    //                var data = [];
-    //                for (var i = l; i < l + 4; i++) {
-    //                    var height = ~~(Math.random() * 500) + 100;
-    //                    var width = 280;
-    //                    var padding = (height/width)*100;
-    //                    var id = ~~(Math.random() * 10000);
-    //                    data.push({"url":'http://lorempixel.com/g/'+ width + '/' + height + '/?' + id, "height" : height, "width":width, "padding":padding});
-    //                }
-    //                $scope.items = $scope.items.concat(data);
-    //                $scope.$broadcast('scroll.infiniteScrollComplete');
-    //            }
-    //        }
-    //    };
-    //
-    //    $scope.$on('$stateChangeSuccess', function() {
-    //        console.log("state Change Success");
-    //        $scope.loadMore();
-    //    });
-    //});
--- www/js/services.js
+++ www/js/services.js
@@ -1,44 +1,58 @@
 angular.module('starter.services', [])
 
-.factory('Camera', ['$q', function($q) {
- 
-  return {
-    getPicture: function(options) {
-      var q = $q.defer();
-      
-      navigator.camera.getPicture(function(result) {
-        // Do any magic you need
-        q.resolve(result);
-      }, function(err) {
-        q.reject(err);
-      }, options);
-      
-      return q.promise;
-    }
-  }
-}]);
+    .factory('Camera', ['$q', function ($q) {
 
-//
-//.factory('Placehold', function () {
-//
-//  var Placehold = function () {
-//    //this.busy = false;
-//    this.images = [];
-//  };
-//
-//  Placehold.prototype.nextPage = function ($scope) {
-//    if (this.busy) return;
-//    //this.busy = true;
-//
-//    var last = this.images[this.images.length - 1];
-//    for (var i = 1; i <= 10; i++) {
-//      var height = ~~(Math.random() * 500) + 100;
-//      var id = ~~(Math.random() * 10000);
-//      this.images.push('http://lorempixel.com/g/280/' + height + '/?' + id);
-//    }
-//    //this.busy = false;
-//    $scope.$broadcast('scroll.inifiniteScrollComplete');
-//  };
-//
-//  return Placehold;
-//});
+        return {
+            getPicture: function (options) {
+                var q = $q.defer();
+
+                navigator.camera.getPicture(function (result) {
+                    // Do any magic you need
+                    q.resolve(result);
+                }, function (err) {
+                    q.reject(err);
+                }, options);
+
+                return q.promise;
+            }
+        }
+    }])
+
+
+    .factory('Gallery', function () {
+
+        var Gallery = function () {
+            this.photos = [];
+            this.more = true;
+        };
+
+        Gallery.prototype.loadMorePhotos = function () {
+            console.log('loadMorePhotos');
+            if (this.more) {
+                console.log("need more");
+                var l = this.photos.length;
+                if (l >= 60) {
+                    this.more = false;
+                    console.log("no More Items Available");
+                } else {
+                    var data = [];
+                    for (var i = l; i < l + 2; i++) {
+                        var height = ~~(Math.random() * 500) + 100;
+                        var width = 280;
+                        var padding = (height / width) * 100;
+                        var id = ~~(Math.random() * 10000);
+                        data.push({
+                            "url": 'http://lorempixel.com/g/' + width + '/' + height + '/?' + id,
+                            "height": height,
+                            "width": width,
+                            "padding": padding
+                        });
+                    }
+                    this.photos = this.photos.concat(data);
+                    console.log('photos size : ' + this.photos.length);
+                }
+            }
+        };
+
+        return Gallery;
+    });
